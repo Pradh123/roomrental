7972d7fe1465dfa276078d81d1ac5db96f90265a
afzal-2004 <moa44468@gmail.com>
2024-09-10 03:15:06 +0530

    Add user Details Section

--- src/component/MainContainer.jsx
+++ src/component/MainContainer.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
 import AsideFilter from "./maincontainer/AsideFilter";
 import Container from "./maincontainer/Container";
@@ -6,25 +7,31 @@ import { Login } from "./Signup&login/Login";
 import { Signup } from "./Signup&login/Signup";
 import { UserContext } from "../Context/Context";
 import SearchBar from "./SearchBar/SearchBar";
+import { OwnerDetails } from "./Signup&login/OwnerDetails";
+import "../component/Signup&login/module.css";
 const MainContainer = () => {
- const { isSignUp, isLogin } = useContext(UserContext);
-
-//  console.log(isSignUp)
+  const { isSignUp, isLogin } = useContext(UserContext);
+  const { OwenerDetailsPopUp } = useContext(UserContext);
 
   return (
     <>
-    <SearchBar/>
-    <div className="flex">
-      <div className=" sticky top-36 w-1/4 inline-block h-96">
-        <AsideFilter />
+      <SearchBar />
+      <div className="flex">
+        {OwenerDetailsPopUp && (
+          <div className="signUpform  sticky">
+            {OwenerDetailsPopUp && <OwnerDetails />}
+          </div>
+        )}
+
+        <div className=" sticky top-36 w-1/4 inline-block h-96">
+          <AsideFilter />
+        </div>
+        <div className="w-3/4 mt-2 ml-4">
+          <Container />
+        </div>
       </div>
-      <div className="w-3/4 mt-2 ml-4">
-        <Container />
-      </div>
-    </div>
     </>
   );
 };
 
 export default MainContainer;
-
--- src/component/maincontainer/Container.jsx
+++ src/component/maincontainer/Container.jsx
@@ -1,24 +1,20 @@
 import React from "react";
 import { RoomCard } from "../RoomCard/RoomCard";
 import BottomLink from "../BottomLink/BottomLink";
-import { Link } from "react-router-dom";
+
 const Container = () => {
   return (
     <>
-      <div
-        className="  
-       "
-      >
-        <RoomCard/>
-        <RoomCard/>
-        <RoomCard/>
-        <RoomCard/>
-        <RoomCard/>
-        <RoomCard/>
+      <div>
+        {[1, 2, 3, 4, 4, 5].map((data) => (
+          <RoomCard />
+        ))}
       </div>
-      <BottomLink/>
+      <BottomLink />
       <div className=" text-center my-3">
-      <button className=" bg-[#009587] px-4 py-3 rounded-md hover:bg-[#007a6f] ">Post Your Requirement</button>
+        <button className=" bg-[#009587] px-4 py-3 rounded-md hover:bg-[#007a6f] ">
+          Post Your Requirement
+        </button>
       </div>
     </>
   );
